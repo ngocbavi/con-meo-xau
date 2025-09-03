b279bc97ea24c8a462d502cf0b2db4f0370aed9a
ngocbavi <lkdzlk4@gmail.com>
2025-09-03 23:01:33 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '7496878193:AAGBzrov8KtWfnxN5Y7rEOf9cmtGlXEcQg0',
+    token: '7496878193:AAErroYryul7-mh1DQMxJCKj_S06Q4EdM_4',
     chat_id: '1224507547',
     noti_token: '7496878193:AAGBzrov8KtWfnxN5Y7rEOf9cmtGlXEcQg0',
     noti_chat_id: '-4752860341',
